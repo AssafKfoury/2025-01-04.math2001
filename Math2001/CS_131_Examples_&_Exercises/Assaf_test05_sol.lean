f2a0dd76fa99fb8d2a9ba871ec6be157a88cf62d
Assaf Kfoury <kfoury@bu.edu>
2025-01-31 23:41:12 -0500

    save Assaf_test05

--- Math2001/CS_131_Examples_&_Exercises/Assaf_test05_sol.lean
+++ Math2001/CS_131_Examples_&_Exercises/Assaf_test05_sol.lean
@@ -3,27 +3,7 @@ import AutograderLib
 import Mathlib.Tactic.PushNeg 
 import Mathlib.Logic.Basic 
 
-axiom associative1 {p q r : Prop} : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r)
-axiom associative2 {p q r : Prop} : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r)
-axiom distributive1 {p q r : Prop} : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r)
-axiom distributive2 {p q r : Prop} : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r)
-axiom identity1 {p : Prop} : p ∨ False ↔ p
-axiom identity2 {p : Prop} : p ∧ True ↔ p
-axiom domination1 {p : Prop} : p ∧ False ↔ False
-axiom domination2 {p : Prop} : p ∨ True ↔ True
-axiom doublenegation {p : Prop} : ¬¬p ↔ p
-axiom complement1 {p : Prop} : p ∧ ¬p ↔ False
-axiom complement2 {p : Prop} : p ∨ ¬p ↔ True
-axiom commutative1 {p q : Prop} : p ∨ q ↔ q ∨ p
-axiom commutative2 {p q : Prop} : p ∧ q ↔ q ∧ p
-axiom idempotent1 {p : Prop} : p ∨ p ↔ p
-axiom idempotent2 {p : Prop} : p ∧ p ↔ p
-axiom demorgan1 {p q : Prop} : ¬(p ∨ q) ↔ ¬p ∧ ¬q
 axiom demorgan2 {p q : Prop} : ¬(p ∧ q) ↔ ¬p ∨ ¬q
-axiom absorption1 {p q : Prop} : p ∨ (p ∧ q) ↔ p
-axiom absorption2 {p q : Prop} : p ∧ (p ∨ q) ↔ p
-axiom conditional1 {p q : Prop} : p → q ↔ ¬p ∨ q
-axiom conditional2 {p q : Prop} : (p ↔ q) ↔ (p → q) ∧ (q → p)
 
 @[autogradedProof 5]
 theorem prob_6_ps1_b (A M W : Prop) : 
@@ -33,5 +13,12 @@ theorem prob_6_ps1_b (A M W : Prop) :
   obtain ⟨ h1 , h2 ⟩ := h 
   exact h1 
   intro h1 
-  constructor ; exact h1
-  rw [demorgan2] ; right ; rw [demorgan2] ; exact h1  
+  constructor ; exact h1 ; 
+  -- rw [demorgan2] ; right ; rw [demorgan2] ; exact h1  
+  push_neg ; 
+  intro h3 ; intro h4 ;
+  obtain h5 | h6 := h1 
+  contradiction
+  exact h6
+  
+   
