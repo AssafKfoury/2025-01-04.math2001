55284dca2e4bb063e7c462cb72142c6fe114454b
Assaf Kfoury <kfoury@bu.edu>
2025-02-06 19:31:50 -0500

    adjust ps2.lean

--- Math2001/CS_131_Examples_&_Exercises/ps2.lean
+++ Math2001/CS_131_Examples_&_Exercises/ps2.lean
@@ -59,11 +59,11 @@ theorem ref3 {n : ℤ} (hn : Even n) : Odd (n ^ 2 + 2 * n - 5) := by
 
 @[autogradedProof 5]
 theorem problem4 {x y : ℤ} : Odd (x + y) → Odd (x ^ 2 + y ^ 2) := by
-  have h_xy : x ^ 2 + y ^ 2 = (x + y) ^ 2 - 2*x*y := by ring
-  rw [h_xy]
+  -- have h_xy : x ^ 2 + y ^ 2 = (x + y) ^ 2 - 2*x*y := by ring
+  -- rw [h_xy]
   sorry 
 
-  done  
+    
 
 /- # You can use the tactic contrapose to prove the contrapositive instead.
    # Also, you can use rw [← Int.odd_iff_not_even] to convert proving
@@ -74,14 +74,14 @@ theorem problem4 {x y : ℤ} : Odd (x + y) → Odd (x ^ 2 + y ^ 2) := by
 -/
 @[autogradedProof 5] 
 theorem problem5 {a : ℤ} : Even ((a + 1) ^ 2) → Odd (a) := by
-  contrapose
-  intro h
-  rw [← Int.odd_iff_not_even]
-  rw [← Int.even_iff_not_odd] at h
+  -- contrapose
+  -- intro h
+  -- rw [← Int.odd_iff_not_even]
+  -- rw [← Int.even_iff_not_odd] at h
   --    in the Lean_4 Playground you may wish to use the 
   --    next two lines instead of the preceding two lines.
   -- rw [Int.not_even_iff_odd]
   -- rw [Int.not_odd_iff_even] at h
   sorry
 
-  done
+  
