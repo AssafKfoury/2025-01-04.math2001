36a9bc9648dd44b4d3dceebd1e566aef4c1d9d2e
Assaf Kfoury <kfoury@bu.edu>
2025-04-23 09:37:06 -0400

    aaa

--- Math2001/CS_131_Examples_&_Exercises/Assaf_ps10_prelim.lean
+++ Math2001/CS_131_Examples_&_Exercises/Assaf_ps10_prelim.lean
@@ -23,7 +23,7 @@ def F : ℕ → ℚ -- recursive def of (n+3)! / (3! * n!)
   | 0 => 1
   | n + 1 => (F n) * (n + 4) / (n + 1)
 
-def G : ℚ → ℚ -- G is a simpler (non-recursive) def of F
+def G : ℕ → ℚ -- G is a simpler (non-recursive) def of F
   | n => (n + 3) * (n + 2) * (n + 1) / 6
 
 #eval G 1 = 1/2 * ((A 2) + (B 2))
@@ -50,13 +50,13 @@ lemma sum_B (n : ℕ) : B n = n * (n + 1) * (2 * n + 1) / 6 := by
             _   = k * (k+1) * (2*k + 1) / 6 + (k+1) * (k+1) := by rw [IH]
             _   = (k+1) * (k+1+1) * (2 * (k+1) + 1) / 6 := by ring
 
-theorem problem3 (n : ℕ) : G (n:ℚ) = 1 / 2 * (A (n  + 1) + B (n + 1)) := by
-  -- have h_A : A n = n * (n + 1) / 2 := by apply (sum_A n)
-  -- have hA : A (n+1) = (n+1) * (n + 2) / 2 := by apply? -- exact  sum_A (n+1)
-  calc G n = (n + 3) * (n + 2) * (n + 1) / 6 := by rw [G]
-     _ = (n * n * n + 6 * n * n + 11 * n + 6) / 6 := by ring
-     _ = (2 * n * n * n + 12 * n * n + 22 * n + 12) / 12 := by ring
-     _ = (3 * n * n + 9 * n + 6) / 12 + (2 * n * n * n + 9 * n * n + 13 * n +6) / 12 := by ring
-     _ = (n * n + 3 * n + 2) / 4 + (2 * n * n * n + 9 * n * n + 13 * n + 6) / 12 := by ring
-     _ = (1/2) * ((n + 1) * (n + 2) / 2 + (n + 1) * (n + 2) * (2 * n + 3) / 6) := by ring
-     _ = (1/2) * (A (n+1) + (n + 1) * (n + 2) * (2 * n + 3) / 6) := by apply (sum_A (n+1))
+/-
+theorem problem3 (n : ℕ) : G n = 1 / 2 * (A (n  + 1) + B (n + 1)) := by
+  simple_induction n with k IH
+  · -- base case
+    calc G 0 = (0 + 3) * (0 + 2) * (0 + 1) / 6 := by exact rfl
+           _ = 1 := by ring
+           _ = (1/2) * (1 + 1) := by ring
+           _ = (1/2) * (1 * (1 + 1) / 2 + 1) := by ring
+           _ = (1/2) * ((A 1) )
+-/
