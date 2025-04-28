e809bddf6d214d64d7655c1bbd5e2a2185d99348
AssafKfoury <kfoury@bu.edu>
2025-04-28 07:27:59 -0400

    aaa

--- Math2001/CS_131_Examples_&_Exercises/Assaf_ps11_prelim.lean
+++ Math2001/CS_131_Examples_&_Exercises/Assaf_ps11_prelim.lean
@@ -1,5 +1,6 @@
 import Mathlib.Data.Real.Basic
 import Library.Basic
+
 -- import Init.Data.Nat.Defs
 import Init.Data.Fin
 import Std.Data.List.Basic
@@ -11,6 +12,8 @@ import Mathlib.Data.List.Basic
 -- import Mathlib.Data.List.Lemmas
 import Std.Data.List.Lemmas
 
+open Function
+open Set
 
 -- import Mathlib.Tactic.Qify
 -- import Mathlib.Tactic.Zify
@@ -30,8 +33,8 @@ def oddN (n : ℕ) : Bool := ¬ (2 ∣ n)
 def cond : Bool → ℕ → ℕ → ℕ
   | true, x, y => x
   | false, x, y => y
-/- ## two_from_three chooses 2 from 3 nat numbers whose sum is even -/
-def two_from_three (x y z : ℕ) : ℕ :=
+/- ## first_2_in_3 chooses first 2 in 3 nat numbers whose sum is even -/
+def first_2_in_3 (x y z : ℕ) : ℕ :=
   cond (((evenN x) ∧ (evenN y)) ∨ ((oddN x) ∧ (oddN y))) (x + y)
     (cond (((evenN x) ∧ (evenN z)) ∨ ((oddN x) ∧ (oddN z))) (x + z)
       (cond (((evenN y) ∧ (evenN z)) ∨ ((oddN y) ∧ (oddN z))) (y + z) (y + z)
@@ -39,8 +42,35 @@ def two_from_three (x y z : ℕ) : ℕ :=
 
 #eval (oddN 3)
 #eval (evenN 3) ∧ (evenN 4)
-#eval two_from_three 5 6 7
+#eval first_2_in_3 5 6 9
+#check evenN
+-- #eval Finset.ofList [1,2,3]
 
+def PositiveIntSet1 : Type  := { n : ℕ | 0 < n }
+def PositiveIntSet2 : Set ℕ := { n : ℕ | 0 < n }
+#check (3 ∈ PositiveIntSet2)
+-- #eval (3 ∈ PositiveIntSet2)
+#reduce (3 ∈ PositiveIntSet2)
+
+
+def bounded_finset (a b : ℕ) : Finset ℕ :=
+   (Finset.range (b + 1)).filter (λ (x : ℕ) => x ≥ a)
+
+--#eval bounded_finset 2 5 -- Output: {2, 3, 4, 5}
+--#eval bounded_finset 0 3 -- Output: {0, 1, 2, 3}
+--#eval bounded_finset 5 5 -- Output: {5}
+--#eval bounded_finset 6 5 -- Output: {}
+#check 3 ∈ (Finset.range 5)
+#eval 3 ∈ (Finset.range 5)
+#eval 13 ∈ (bounded_finset (2^3) (20*15))
+#eval 2^3
+#check fun (x : Int) => x + 1
+
+
+def pairs (X Y : Set ℕ) : Set (ℕ × ℕ) := { (x,y) | (x ∈ X) (y ∈ Y) }
+def evens : Set ℕ := { x | ∃ (k : ℕ), x = 2 * k }
+def odds  : Set ℕ := { x | ∃ (k : ℕ), x = 2 * k + 1 }
+def EvensOdds := pairs evens odds
 /-
 def random_set_of_k_distinct_nats (k n : Nat) : IO (Finset Nat) := do
   if k > n then
@@ -49,12 +79,12 @@ def random_set_of_k_distinct_nats (k n : Nat) : IO (Finset Nat) := do
     let mut result : Finset Nat := {}
     let mut i : Nat := 0
     while i < k do
-      let rand_num := IO.randNat (n + 1)
+      let rand_num := StdGen.next -- IO.randNat (n + 1)
       let new_num : Nat ← rand_num
-      if (result.contains new_num) then
+      if  new_num ∈ result then -- (result.contains new_num) then
           continue
       else
-          result := result.insert new_num
+          result := insert new_num result -- result.insert new_num
           i := i + 1
     return result
 
