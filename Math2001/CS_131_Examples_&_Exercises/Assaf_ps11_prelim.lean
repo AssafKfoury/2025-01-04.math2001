36942f6a5df7ed1d9b6ef9f971893c63677ec9bb
AssafKfoury <kfoury@bu.edu>
2025-04-29 22:55:36 -0400

    aaa

--- Math2001/CS_131_Examples_&_Exercises/Assaf_ps11_prelim.lean
+++ Math2001/CS_131_Examples_&_Exercises/Assaf_ps11_prelim.lean
@@ -1,5 +1,10 @@
 import Mathlib.Data.Real.Basic
 import Library.Basic
+-- import Mathlib.Tactic.Qify
+-- import Mathlib.Tactic.Zify
+import Library.Tactic.ModEq
+-- import Mathlib.Tactic
+-- import AutograderLib
 
 -- import Init.Data.Nat.Defs
 import Init.Data.Fin
@@ -13,8 +18,6 @@ import Mathlib.Data.List.Basic
 import Std.Data.List.Lemmas
 
 import Mathlib.Data.Set.Finite -- needed for Finset.card
-
-
 --import Mathlib.Data.Finset.Basic -- /- ## DOES NOT CAUSE ERROR -/
 import Mathlib.Algebra.BigOperators.Basic -- NEEDED FOR Finset.sum
 
@@ -23,31 +26,54 @@ namespace Int
 set_option trace.Meta.Tactic.simp true -- hilights tactic
                                        -- 'simp' wherever it is used?
 
-/- ## NONE OF THE FOLLOWING SEEM NEEDED, THOUGH THEY DON'T CAUSE ERRORS -/
+/- ## NONE OF THE FOLLOWING SEEMS NEEDED, THOUGH THEY DON'T CAUSE ERRORS -/
 --open Finset
 --open BigOperators
 --open Function
---open Set
+open Set
 
+/- ## Define an infinite set -/
+   def myInfSet : Set ℕ := { n : ℕ | 0 < n }
 /- ## Define a finite set -/
    def mySet : Finset ℕ := {1, 2, 3, 4}
 /- ## Define a function the square function -/
    def myFunction (n : ℕ) : ℕ := n * n
-/- ## Calculate the sum using Finset.sum -/
+/- ## Calculate sum of the squares using Finset.sum -/
    def sumOfSquares : ℕ := Finset.sum mySet myFunction
 /- ## Print the result -/
 #eval sumOfSquares -- Output: 30
 
 #check Finset.range
 #eval Finset.card mySet
+#eval 3 ∈ mySet
+#reduce 3 ∈ myInfSet -- #eval 3 ∈ myInfSet -- CAUSES ERROR!!!
 #eval Finset.sum  ({1, 2, 3, 4}) (fun x => x)
 #eval Finset.sum  ({1, 2, 3, 4}) (fun x => x * x)
 
--- import Mathlib.Tactic.Qify
--- import Mathlib.Tactic.Zify
--- import Library.Tactic.ModEq
--- import Mathlib.Tactic
--- import AutograderLib
+/- ## Calculate sum of entries in finite set S of nats using Finset.sum -/
+   def sumOfAll (S : Finset ℕ) : ℕ := Finset.sum S (fun (x : ℕ) => x)
+#eval sumOfAll {1,2,3,4}
+
+/- ## boundedFinset creates finite set of numbers
+   ## from a (inclusive) to b (inclusive) -/
+   def boundedFinset (a b : ℕ) : Finset ℕ :=
+      (Finset.range (b + 1)).filter (λ (x : ℕ) => x ≥ a)
+
+#check 3 ∈ (Finset.range 5)
+#eval 5 ∈ (Finset.range 5)
+#eval 7 ∈ (boundedFinset (2^3) (2*15))
+#eval Finset.card (boundedFinset (2^3) (20*15))
+#eval Finset.card (boundedFinset (2^3) (2*15))
+#eval 7 ∈ (boundedFinset (2^3) (2*15))
+#eval 2^3
+#check fun (x : ℤ) => x + 1
+#eval Finset.sum {1, 2, 3, 4} (fun (x : ℤ) => x)
+
+/- ## Other useful sets of natural numbers -/
+   def pairs (X Y : Set ℕ) : Set (ℕ × ℕ) := { (x,y) | (x ∈ X) (y ∈ Y) }
+   def evens : Set ℕ := { x | ∃ (k : ℕ), x = 2 * k }
+   def odds  : Set ℕ := { x | ∃ (k : ℕ), x = 2 * k + 1 }
+   def EvensOdds := pairs evens odds
 
 /- ## evenN and oddN test whether a natural number is even or odd -/
     def evenN (n : ℕ) : Bool := (2 ∣ n)
@@ -74,33 +100,15 @@ set_option trace.Meta.Tactic.simp true -- hilights tactic
 #eval (QntRdr_by_2_pow_n 14 3)
 #eval (2^3)*(QntRdr_by_2_pow_n 14 3).1 + (QntRdr_by_2_pow_n 14 3).2
 
-def PositiveIntSet1 : Type  := { n : ℕ | 0 < n }
-def PositiveIntSet2 : Set ℕ := { n : ℕ | 0 < n }
-#check (3 ∈ PositiveIntSet2)
--- #eval (13 ∈ PositiveIntSet2) -- doesn't work!!!
-#reduce (13 ∈ PositiveIntSet2)
-
-/- ## bounded_finset creates finite set of numbers
-   ## from a (inclusive) to b (inclusive) -/
-def bounded_finset (a b : ℕ) : Finset ℕ :=
-   (Finset.range (b + 1)).filter (λ (x : ℕ) => x ≥ a)
-
-#check 3 ∈ (Finset.range 5)
-#eval 5 ∈ (Finset.range 5)
-#eval 7 ∈ (bounded_finset (2^3) (2*15))
-#eval Finset.card (bounded_finset (2^3) (20*15))
-#eval Finset.card (bounded_finset (2^3) (2*15))
-#eval 7 ∈ (bounded_finset (2^3) (2*15))
-#eval 2^3
-#check fun (x : Int) => x + 1
--- #eval Finset.sum {1, 2, 3, 4} (fun x => x)
-
-def pairs (X Y : Set ℕ) : Set (ℕ × ℕ) := { (x,y) | (x ∈ X) (y ∈ Y) }
-def evens : Set ℕ := { x | ∃ (k : ℕ), x = 2 * k }
-def odds  : Set ℕ := { x | ∃ (k : ℕ), x = 2 * k + 1 }
-def EvensOdds := pairs evens odds
 /-
-def random_set_of_k_distinct_nats (k n : Nat) : IO (Finset Nat) := do
+theorem foo1 (S : Finset ℕ) :
+    ∀ (T : Finset ℕ), (T ⊆ S) → (sumOfAll T) ≤ (sumOfAll S) := by
+    intro h ; intro hS ;
+    -- dsimp [Set.subset_def] at
+-/
+
+/-
+def random_set_of_k_distinct_nats (k n : ℕ) : IO (Finset ℕ) := do
   if k > n then
     return {}
   else
