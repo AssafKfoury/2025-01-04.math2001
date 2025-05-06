db5f5441640fbbaa695f3482986a08bd57a24515
Assaf Kfoury <kfoury@bu.edu>
2025-05-06 13:54:30 -0400

    aaa

--- Math2001/CS_131_Examples_&_Exercises/Assaf_ps11_test_2.lean
+++ Math2001/CS_131_Examples_&_Exercises/Assaf_ps11_test_2.lean
@@ -2,7 +2,7 @@ import Mathlib.Data.Set.Basic -- needed for "def A"
 import Mathlib.Data.Finset.Basic -- needed for "lemma instA"
 --import Mathlib.Data.Set.Finite
 
-/- ## Example 1 in "Working with integer sets in Lean 4":
+/- ## EXAMPLE 1 in "Working with integer sets in Lean 4":
       https://brandonrozek.com/blog/integer-sets-lean4/  -/
 
 def A : Set ℤ := {x : Int | x^2 = 9}
@@ -34,19 +34,16 @@ instance (S : Finset ℤ) : Decidable (↑S ⊆ A) := by
 #check Set.subset_def -- ({1,2} ⊆ {1,2,3})
 
 lemma instA : A = ({3, -3} : Finset ℤ) := by
-  let S : Finset ℤ := {3, -3}    -- declaration placed in the context
+  let S : Finset ℤ := {3, -3}
   change (A = ↑S)
-  rw [Set.Subset.antisymm_iff]
-  -- ## Assaf: preceding line instead of the next three
-  -- suffices A ⊆ ↑S ∧ ↑S ⊆ A by
-  --   rewrite [Set.Subset.antisymm_iff]
-  --   assumption
+  suffices A ⊆ ↑S ∧ ↑S ⊆ A by
+    rewrite [Set.Subset.antisymm_iff]
+    assumption
 
   have H1 : A ⊆ ↑S := by
-    dsimp [Set.subset_def] -- added by Assaf
     intro (n : ℤ)
     -- Goal is now (n ∈ A → n ∈ {3, -3})
-    intro H1_1             -- instead of 'intro (H1_1: n ∈ A)'
+    intro (H1_1: n ∈ A)
     have H1_1 : n^2 = 9 := by
       rewrite [A, Set.mem_setOf_eq] at H1_1
       assumption
@@ -61,3 +58,29 @@ lemma instA : A = ({3, -3} : Finset ℤ) := by
   have H2 : ↑S ⊆ A := by decide
 
   exact And.intro H1 H2
+
+lemma instA_Assaf : A = ({3, -3} : Finset ℤ) := by
+  let S : Finset ℤ := {3, -3}    -- declaration placed in the context
+  change (A = ↑S)
+  rw [Set.Subset.antisymm_iff]
+  constructor
+
+  have H1 : A ⊆ ↑S := by
+    dsimp [Set.subset_def] -- added by Assaf
+    intro (n : ℤ)
+    -- Goal is now (n ∈ A → n ∈ {3, -3})
+    intro H1_1             -- instead of 'intro (H1_1: n ∈ A)'
+    have H1_1 : n^2 = 9 := by
+      rw [A, Set.mem_setOf_eq] at H1_1
+      exact H1_1
+
+    suffices n = 3 ∨ n = -3 by
+      show n ∈ S
+      rw [Finset.mem_insert, Finset.mem_singleton]
+      exact this
+
+    exact eq_or_eq_neg_of_sq_eq_sq n 3 H1_1
+  exact H1
+
+  have H2 : ↑S ⊆ A := by decide
+  exact H2
