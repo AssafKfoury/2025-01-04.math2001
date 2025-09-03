aa37c0cac53edc1ec20f6c5c780a773a5d1562c3
Assaf Kfoury <kfoury@bu.edu>
2025-09-03 14:08:42 -0400

    aaa

--- Math2001/CS_131_Examples_&_Exercises/Assaf_Labs_in_CS_511_Fall2025/lab01_prelim.lean
+++ Math2001/CS_131_Examples_&_Exercises/Assaf_Labs_in_CS_511_Fall2025/lab01_prelim.lean
@@ -6,3 +6,15 @@ import Library.Basic
 -- and carry out arithmetical operations, as it contains fundamental
 -- definitions and operations on integers:
 open Int
+
+#check 3 --
+
+example {p q : Prop} (h_p : p) : q → (p ∧ q) := by
+  intro h_q
+  constructor
+  apply h_p
+  apply h_q
+
+theorem lem {p q : Prop} (h : p) : q → (p ∧ q) := by
+   intro h_q
+   apply And.intro h h_q
