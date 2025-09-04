821bf9eeb5e5d5e0563f677e0bbd82156648bc0f
AssafKfoury <kfoury@bu.edu>
2025-09-04 09:56:47 -0400

    aaa

--- Math2001/CS_131_Examples_&_Exercises/Assaf_Labs_in_CS_511_Fall2025/lab02_prelim.lean
+++ Math2001/CS_131_Examples_&_Exercises/Assaf_Labs_in_CS_511_Fall2025/lab02_prelim.lean
@@ -1,4 +1,4 @@
-/- 3 Sept 2025 -/
+/- ?? Sept 2025 -/
 import Library.Basic
 
 -- 'Int' is the name of a module, part of the Lean4 standard
@@ -6,15 +6,3 @@ import Library.Basic
 -- and carry out arithmetical operations, as it contains fundamental
 -- definitions and operations on integers:
 open Int
-
-#check 3 --
-
-example {p q : Prop} (h_p : p) : q → (p ∧ q) := by
-  intro h_q
-  constructor
-  apply h_p
-  apply h_q
-
-theorem lem {p q : Prop} (h : p) : q → (p ∧ q) := by
-   intro h_q
-   apply And.intro h h_q
