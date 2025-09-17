0ff5072d54dd4004cd9cdeb34b309084dcce559c
Assaf Kfoury <kfoury@bu.edu>
2025-09-17 13:16:37 -0400

    aaaa

--- Math2001/Homework/CS131_&_CS511_Scripts/Assaf_Labs_in_CS_511_Fall2025/lab03_prelim.lean
+++ Math2001/Homework/CS131_&_CS511_Scripts/Assaf_Labs_in_CS_511_Fall2025/lab03_prelim.lean
@@ -9,7 +9,14 @@ math2001_init
 namespace Nat
 
 example (n : ℕ) : 2 ^ n ≥ n + 1 := by
-  sorry
+  simple_induction n with k IH
+  · -- base case
+    sorry -- numbers
+  · -- inductive step
+    calc 2 ^ (k + 1) = 2 * 2 ^ k := by ring
+      _ ≥ 2 * (k + 1) := by rel [IH]
+      _ = (k + 1 + 1) + k := by ring
+      _ ≥ k + 1 + 1 := by extra
 
 example {n : ℕ} (hn : 2 ≤ n) : (3:ℤ) ^ n ≥ 2 ^ n + 5 := by
   sorry
@@ -18,4 +25,17 @@ theorem problem1 (n : ℕ) : 3 ^ n ≥ n ^ 2 + n + 1 := by
   sorry
 
 theorem problem2 : forall_sufficiently_large n : ℕ, 2 ^ n ≥ n ^ 2 + 4 := by
-  sorry
+  dsimp
+  use 5
+  intros x hx
+  induction_from_starting_point x, hx with k hk IH
+  · numbers
+  · calc
+      2 ^ (k + 1) = 2 * 2 ^ k := by ring
+      _ ≥ 2 * (k ^ 2 + 4) := by rel [IH]
+      _ = k ^ 2 + 4 + k * k + 4 := by ring
+      _ ≥ k ^ 2 + 4 + 5 * k + 4 := by rel [hk]
+      _ = k ^ 2 + 2 * k + 5 + (3 * k + 3) := by ring
+      _ ≥ k ^ 2 + 2 * k + 5 + (3 * 5 + 3) := by rel [hk]
+      _ ≥ k ^ 2 + 2 * k + 5 := by extra
+      _ = (k + 1) ^ 2 + 4 := by ring
