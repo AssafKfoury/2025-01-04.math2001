24da6948e09f6a3998a524ef3516aefdca83dd1e
Assaf Kfoury <kfoury@bu.edu>
2025-09-17 08:39:24 -0400

    aaa

--- Math2001/Homework/CS131_&_CS511_Scripts/Assaf_Labs_in_CS_511_Fall2025/lab03_prelim.lean
+++ Math2001/Homework/CS131_&_CS511_Scripts/Assaf_Labs_in_CS_511_Fall2025/lab03_prelim.lean
@@ -5,6 +5,15 @@ import Mathlib.Data.Real.Basic
 import Library.Tactic.ModEq
 import Library.Basic
 
+math2001_init
+namespace Nat
+
+example (n : ℕ) : 2 ^ n ≥ n + 1 := by
+  sorry
+
+example {n : ℕ} (hn : 2 ≤ n) : (3:ℤ) ^ n ≥ 2 ^ n + 5 := by
+  sorry
+
 theorem problem1 (n : ℕ) : 3 ^ n ≥ n ^ 2 + n + 1 := by
   sorry
 
