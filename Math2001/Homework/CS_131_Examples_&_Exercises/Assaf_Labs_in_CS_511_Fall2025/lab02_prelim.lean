d6a7a57cd8ed81e0270f0ca284b5cbfe19a7cd18
Assaf Kfoury <kfoury@bu.edu>
2025-09-16 23:24:50 -0400

    aaa

---
+++ Math2001/Homework/CS_131_Examples_&_Exercises/Assaf_Labs_in_CS_511_Fall2025/lab02_prelim.lean
@@ -0,0 +1,8 @@
+/- ?? Sept 2025 -/
+import Library.Basic
+
+-- 'Int' is the name of a module, part of the Lean4 standard
+-- library. It is useful to open it when you work with integers
+-- and carry out arithmetical operations, as it contains fundamental
+-- definitions and operations on integers:
+open Int
