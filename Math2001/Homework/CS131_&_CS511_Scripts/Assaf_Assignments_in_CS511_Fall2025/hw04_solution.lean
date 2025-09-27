37d659446cb0cb0ec8635315d4dc663d02fc4d02
Assaf Kfoury <kfoury@bu.edu>
2025-09-27 06:44:07 -0400

    aaa

---
+++ Math2001/Homework/CS131_&_CS511_Scripts/Assaf_Assignments_in_CS511_Fall2025/hw04_solution.lean
@@ -0,0 +1,34 @@
+/- CS 511, 26 Sept 2025 -/
+/- solution for Exercise 4 in Homework Assignment 04 -/
+
+import Mathlib.Logic.Basic
+import Mathlib.Tactic.ByContra
+
+lemma prove_implication_negation (p q : Prop) : (p → q) → ¬ (p ∧ ¬ q) := by
+  intro h_pq
+  by_contra h_pnq
+  obtain ⟨ h_p , h_nq ⟩ := h_pnq
+  have h_q : q := h_pq h_p
+  contradiction
+
+lemma prove_negation_implication (p q : Prop) :  ¬ (p ∧ ¬ q) → (p → q) := by
+  intro h_neg_pnq
+  intro h_p
+  by_cases h_q : q
+  exact h_q
+  have h_pnq : (p ∧ ¬ q) := And.intro h_p h_q
+  contradiction
+
+example {p q : Prop} : (p → q) → (¬p ∨ q)  := by
+  intro h_pq
+  have h_neg_pnq : ¬ (p ∧ ¬ q) := by
+     apply prove_implication_negation
+     exact h_pq
+  sorry
+
+example {p q : Prop} : (¬q → ¬p) → (p → q) := by
+  intro h_nqnp
+  apply prove_negation_implication
+
+
+example {p q : Prop} : (((p → q) → p) → p) := sorry
