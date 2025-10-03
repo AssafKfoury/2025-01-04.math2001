1597f4a3d24d8fc3e2c2f9186938f5903762619d
AssafKfoury <kfoury@bu.edu>
2025-10-03 16:51:04 -0400

    aaa

---
+++ Math2001/Homework/CS131_&_CS511_Scripts/Assaf_Assignments_in_CS511_Fall2025/hw05_template2.lean
@@ -0,0 +1,30 @@
+import Library.Basic
+
+namespace Hidden
+
+inductive List (α : Type μ) where
+  | nil : List α
+  | cons : α → List α → List α
+
+namespace List
+
+def length (as : List α) : ℕ :=
+  match as with
+  | nil       => 0
+  | cons a as => 1 + length as
+
+def append (as bs : List α) : List α :=
+  match as with
+  | nil       => bs
+  | cons a as => cons a (append as bs)
+
+def reverse (as : List α) : List α :=
+  match as with
+  | nil       => nil
+  | cons a as => append (reverse as) (cons a nil)
+
+theorem problem1 (as bs : List α) : length (append as bs) = length as + length bs := by
+  sorry
+
+theorem problem2 (as : List α) : length (reverse as) = length as := by
+  sorry
