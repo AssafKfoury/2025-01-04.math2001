2470edf87f4e59aa48610472f42a2264541ca7db
Assaf Kfoury <kfoury@bu.edu>
2025-10-09 00:03:33 -0400

    aaa

--- Math2001/Homework/CS131_&_CS511_Scripts/Assaf_Assignments_in_CS511_Fall2025/definePL.lean
+++ Math2001/Homework/CS131_&_CS511_Scripts/Assaf_Assignments_in_CS511_Fall2025/definePL.lean
@@ -8,7 +8,7 @@ namespace PL
 
 -- Propositional logic
 inductive PL_wff where
-| var : Nat → PL_wff
+| var : ℕ → PL_wff
 | not : PL_wff → PL_wff
 | and : PL_wff → PL_wff → PL_wff
 | or  : PL_wff → PL_wff → PL_wff
@@ -22,18 +22,30 @@ abbrev myOr  := PL_wff.or
 abbrev myImp := PL_wff.imp
 abbrev myIff := PL_wff.iff
 
-#check PL_wff.var 3
+#check PL_wff.var
 #check myVar 3
 
-abbrev Valuation := Nat → Bool
+def disj_Comm (i j : ℕ) : PL_wff :=
+   myImp (myOr (myVar i) (myVar j)) (myOr (myVar j) (myVar i))
+def deMorgan_4 (i j : ℕ): PL_wff :=
+   myImp (myNot (myAnd (myVar i) (myVar j))) (myOr (myNot (myVar i)) (myNot (myVar j)))
+
+abbrev Valuation := ℕ → Bool
+
+def valA (i : ℕ) : Bool := if (i % 2) = 0 then true else false
+def valB (i : ℕ) : Bool := if (i % 2) = 0 then false else true
 
 def evalProp : PL_wff → Valuation → Bool
-  | .var i    => fun v => v i
-  | .not φ    => fun v => !(evalProp φ v)
-  | .and φ ψ  => fun v => evalProp φ v && evalProp ψ v
-  | .or φ ψ   => fun v => evalProp φ v || evalProp ψ v
-  | .imp φ ψ  => fun v => !evalProp φ v || evalProp ψ v
-  | .iff φ ψ  => fun v => evalProp φ v == evalProp ψ v
+  | .var i    => (fun v => v i)
+  | .not φ    => (fun v => ! (evalProp φ v))
+  | .and φ ψ  => (fun v => evalProp φ v && evalProp ψ v)
+  | .or φ ψ   => (fun v => evalProp φ v || evalProp ψ v)
+  | .imp φ ψ  => (fun v => !evalProp φ v || evalProp ψ v)
+  | .iff φ ψ  => (fun v => evalProp φ v == evalProp ψ v)
+
+#eval evalProp (myVar 2) valA
+#eval evalProp (disj_Comm 1 2) valA
+#eval evalProp (deMorgan_4 10 20) valA
 
 -- Test examples
 def φ1 : PL_wff := .not (.var 0)
