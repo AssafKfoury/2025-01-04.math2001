5bc9cd2e4c71d1bca5a1665b5bcefc39d4482739
AssafKfoury <kfoury@bu.edu>
2025-10-24 17:40:59 -0400

    aaa

--- Math2001/Homework/CS131_&_CS511_Scripts/Assaf_Assignments_in_CS511_Fall2025/hw08_solution.lean
+++ Math2001/Homework/CS131_&_CS511_Scripts/Assaf_Assignments_in_CS511_Fall2025/hw08_solution.lean
@@ -2,12 +2,68 @@
 
 import Mathlib.Data.Real.Basic -- needed in order to use tactic `contrapose`
 
-/- This script illustrates how to prove the `cancellation` property for the  myℕural
-   numbers, in two ways: with and without tactic `contrapose`. It also illustrates
-   `structural induction` on an `inductive definition` of the  myℕural numbers.
+import Library.Basic   -- needed for math2001_init
+math2001_init          -- needed to access Macbeth's tactics:
+                       -- `addarith`, `cancel`, `extra`, `numbers`
 
-   # This script does NOT use the pre-defined  myℕural numbers 0, 1, ..., the
-   # pre-defined type ℕ, and the pre-defined namespace  myℕ, from the Lean library.
+/- # Exercise 3 in Homework 08 -/
+/- Consult page 25 in Slides 18 for hints -/
+
+example (h : ∃x : Type, ∀y : Type, (x = y)) : (∀x : Type, ∀y : Type, (x = y)) := by
+  intros x y
+  obtain ⟨c,hc⟩ := h
+  have hx := hc x
+  have hy := hc y
+  rw [hx] at hy
+  exact hy
+
+example : (∃x : Type, ∀y : Type, (x = y)) → (∀v : Type, ∀w : Type, (v = w)) := by
+  intros h v w
+  obtain ⟨x,hx⟩ := h
+  have hv := hx v
+  have hw := hx w
+  rw [hv] at hw
+  exact hw
+
+/- # Exercise 4 in Homework 08 -/
+
+/- Exercise 5.2.7.2 in Macbeth's book [MOP]  -/
+example (P Q : Prop) : (¬P → ¬Q) ↔ (Q → P) := by
+  constructor
+  · intros nPnQ q
+    by_cases p : P
+    · apply p
+    · have contra := nPnQ p
+      contradiction
+  · intros qp np
+    by_cases q : Q
+    · have contra := qp q
+      contradiction
+    · apply q
+
+/- Exercise 5.3.6.9 in Macbeth's book [MOP] -/
+
+example : ¬ (∃ t : ℝ, t ≤ 4 ∧ t ≥ 5) := by
+  push_neg
+  intros t
+  by_cases ht : t ≥ 5
+  · left
+    calc
+      t ≥ 5 := by rel [ht]
+      _ > 4 := by numbers
+  · push_neg at ht
+    right
+    apply ht
+
+
+/- # Problem 2 in Homework 08 -/
+
+/- The rest of this script illustrates how to prove the `cancellation` property for
+   the natural numbers, in two ways: with and without tactic `contrapose`. It also
+   illustrates `structural induction` on an `inductive definition` of the natural numbers.
+
+   # This script does NOT use the pre-defined  natural numbers 0, 1, ..., the
+   # pre-defined type ℕ, and the pre-defined namespace Nat, from the Lean_4 library.
 
 -/
 
