62abb37ab762021fbdbbf44a89a65b7933ec14c7
AssafKfoury <kfoury@bu.edu>
2026-01-21 23:29:01 -0500

    aaa

--- Math2001/CS131_&_CS511_Scripts/AutoG_test02.lean
+++ Math2001/CS131_&_CS511_Scripts/AutoG_test02.lean
@@ -5,6 +5,7 @@ variable (P Q : Prop)
 
 /- # different ways of proving the 3rd deMorgan's "¬ P ∨ ¬ Q → ¬ (P ∧ Q)" -/
 
+@[autogradedProof 10]
 theorem example_2 : ¬ P ∨ ¬ Q → ¬ (P ∧ Q) := by
   intro h
   intro h_pq
